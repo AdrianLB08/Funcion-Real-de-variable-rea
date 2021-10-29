b0b15d6e822325cdbc3aa4dd2344e211c68bed7c
AdrianLB08 <91706214+AdrianLB08@users.noreply.github.com>
2021-10-28 21:34:14 -0500

    Add files via upload

---
+++ raiz.m
@@ -0,0 +1,21 @@
+% Octave script
+% Title              :funcion real de variable real
+% Description        :Scrip para recordar funiones reales 
+% Author             :Adrian Lovera Bombela
+% Date               :28/10/2021
+% Version            :1 
+% Usage              :octave 
+%                    :https://octaveintro.readthedocs.io/en/latest/index.html
+
+%representar la siguiente euacion
+%f(x)=1+ ? x-4
+clear
+pkg load symbolic
+syms x
+x=[4:1:15]
+y= 1+(sqrt(x-4));
+plot(x,y)
+grid on
+ylabel('y')
+xlabel('x')
+title('dominio x>=4, rango yE[1,+oo), minimo (4,1)')
